b0ccef4f21cd008d784d6c028457fc667c9aa1ab
nielsvandenheuvel <nielsvandenheuvel00@gmail.com>
2024-11-06 16:26:17 +0100

    VFI solver viable and policy tracer

--- julia/src/ComputerAssignment1.jl
+++ julia/src/ComputerAssignment1.jl
@@ -2,13 +2,14 @@ module ComputerAssignment1
 
 using Revise
 using Plots
+using LinearAlgebra
 
 #== Problem 1 ==#
 includet("DynamicProgramming/DynamicProgramming.jl")
 import .DynamicProgramming
 
 # Parameters
-α = 0.5
+α = 1.5
 β = 0.9
 δ = 0.9
 T = 100
@@ -17,32 +18,24 @@ T = 100
 z = 1
 
 # Functions
-F(k) = k^α # Production function
-f(k) = α*k^(α-1) # Marginal production
+f(k) = k^α # Production function
 u(c) = c > 0 ? log(c) : -Inf # Instantaneous utility
-c(k, κ) = k*(1 - δ + z*f(k)) - κ > 0 ? k*(1 - δ + z*f(k)) - κ : 1E-5 # Consumption policy function
-ũ(k, κ) = u(c(k, κ)) # Derived utility
+c(k, κ) = f(k) + k*(1 - δ) - κ # Consumption policy function
+U(k, κ) = u(c(k, κ)) # Derived utility
 
 # Initialization
-K  = 0.01:0.01:01
-V₀ = Dict([(k, ũ(k, k)/(1-β)) for k ∈ K])
+k̲ = 0.81
+k̄ = 2.00
+K  = k̲:0.001:k̄
+V₀ = Dict([(k, U(k, k)/(1-β)) for k ∈ K])
 
 # Solve the problem
-V̂, k̂ = DynamicProgramming.solveVFI(ũ, β, V₀; maxiter=100)
+V̂, k̂ = DynamicProgramming.solveVFI(U, β, V₀; maxiter=100)
 
 # Plot value function
-k₀ = 0.7
-T = 100
-k̂₀ = zeros(T,1)
-k̂₀[1] = k₀
-V̂₀ = zeros(T,1)
-V̂₀[1] = V̂[k₀]
-for t ∈ 2:T
-    k̂₀[t] = k̂[k̂₀[t-1]]
-    V̂₀[t] = V̂[k̂₀[t]]
-end
-plot(1:T, k̂₀)
-plot(1:T, V̂₀)
+k₀ = k̲:0.1:k̄
+T  = 50
+plot(1:T, DynamicProgramming.policyTrace(T, k̂, k₀))
 
 
 # Problem 5
--- julia/src/DynamicProgramming/DynamicProgramming.jl
+++ julia/src/DynamicProgramming/DynamicProgramming.jl
@@ -1,11 +1,15 @@
 module DynamicProgramming
 
 using Printf
+using LinearAlgebra
+
+# Type alias for working input
+const FunctionOnGrid = AbstractDict{<:Union{<:Real, <:AbstractVector{<:Real}}, <:Real}
 
 function solveVFI(
     u::Function,
     β::Real,
-    V₀::AbstractDict{<:Union{<:Real, <:AbstractVector{<:Real}}, <:Real};
+    V₀::FunctionOnGrid;
     monotone::Bool = false,
     concave::Bool = false,
     tolerance::Real = 10E-5,
@@ -16,26 +20,30 @@ function solveVFI(
     # Initialize
     i   = 1
     Vᵢ  = V₀
-    θᵢ  = Dict([(θ, θ) for θ ∈ eachindex(Vᵢ)])
+    πᵢ  = Dict([(θ, θ) for θ ∈ eachindex(Vᵢ)])
     ΔVᵢ = tolerance
 
     # Iterate
     while (ΔVᵢ >= tolerance && i <= maxiter)
+        # Measure performance
+        start_time = time_ns()
+
         # Save the state
-        Vᵢ₋₁ = Vᵢ
+        ΔVᵢ  = 0
+        Vᵢ₋₁ = copy(Vᵢ)
         
-        #Hallo
         # Do the grid search
         for θ ∈ keys(Vᵢ)
-            # Initialize search value
-            Vᵢ[θ] = -Inf
             # Loop over potential choices
             for ϑ ∈ keys(Vᵢ)
-                # Evaluate choice value
-                Vₖₗ = u(θ, ϑ) + β*Vᵢ₋₁[ϑ]
-                if (Vₖₗ > Vᵢ[θ])
-                    Vᵢ[θ] = Vₖₗ
-                    θᵢ[θ] = ϑ
+                # No need to check if new value is -Inf
+                if (Vᵢ₋₁[ϑ] > -Inf)
+                    # Evaluate choice value
+                    V̂ = u(θ, ϑ) + β*Vᵢ₋₁[ϑ]
+                    if (V̂ > Vᵢ[θ])
+                        Vᵢ[θ] = V̂
+                        πᵢ[θ] = ϑ
+                    end
                 end
             end
 
@@ -47,12 +55,62 @@ function solveVFI(
 
         # Update
         i += 1
+        end_time = time_ns()
         if (i % (maxiter/10) == 0)
-            println("Iteration $i/$maxiter: slack is $(@sprintf("%.2e", ΔVᵢ))")
+            println("Progress: $(round(i/maxiter*100))% | Slack: $(@sprintf("%.2e", ΔVᵢ)) | ETA: $(round((end_time-start_time)*(maxiter-i)/1e9/60, digits=2))min")
         end
     end
 
-    return Vᵢ, θᵢ
+    return Vᵢ, πᵢ
 end
+
+function policyTrace(T::Integer, π::FunctionOnGrid, θ₀::Real; plot::Bool = false)
+    if (T == 0)
+        return θ₀
+    else
+        # Initialize path vector
+        Π = zeros(abs(T))
+        
+        # Allow to go back in time
+        if (T > 0)
+            # Intialize
+            Π[1] = π[θ₀]
+
+            # Trace out path
+            for t ∈ 2:T
+                Π[t] = π[Π[t-1]]
+            end
+        else
+            # Intialize
+            Π[-T] = [θ for θ ∈ keys(π) if θ .== θ₀][1]
+
+            # Trace out path
+            for t ∈ (T+1):-1
+                Π[-t] = [θ for θ ∈ keys(π) if θ .== Π[1-t]][1]
+            end
+        end
+    end
+
+    # Plot results
+    if (plot)
+
+    end
+
+    return Π
+end
+
+function policyTrace(T::Integer, π::FunctionOnGrid, θ₀::AbstractVector{<:Real})
+    # Initialize path matrix
+    Π = zeros(abs(T), length(θ₀))
+
+    # Fill
+    for (k, θ) ∈ enumerate(θ₀)
+        Π[1:T, k] = policyTrace(T, π, θ)
+    end
+
+    return Π
+end
+
+function policyTrace
 
 end
